0d5aa7fdc2e53a8af9f9cafac6ef951b6976ea97
Katherine <katherinevse@mail.ru>
2023-10-19 11:36:49 +0300

    -

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Использование: %s <имя файла>\n", argv[0]);
-        return 1;
-    }
+void print_file(char *name);
 
-    FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
-        perror("Не удалось открыть файл");
-        return 1;
-    }
+int main(int argc, char **argv) {
+  for (int i = 1; i < argc; i++){ // цикл начинается с argv[1]
+    print_file(argv[i]);           // в argv[0] находится "./s21_cat"
+  }
+  return 0; 
+}
 
-    int c;
-    while ((c = fgetc(file)) != EOF) {
-        putchar(c);
-    }
 
-    fclose(file);
-    return 0;
+void print_file(char *name) {
+  FILE *f = fopen(name, "rt");
+    
+  if (f != NULL) {
+  int c = fgetc(f);
+  while (c != EOF) {
+    putc(c, stdout);  
+    c = fgetc(f);
+  }
+  fclose(f);
+  }
 }
